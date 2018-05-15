dfde12a21e4d190aa7a0de0b2afda6786014244a
SaaSha <saasha.pina@gmail.com>
2018-05-15 02:02:42 -0400

    Updated

--- portfolio/src/components/About/about.js
+++ portfolio/src/components/About/about.js
@@ -15,13 +15,13 @@ class About extends React.Component {
                 <h1 className="ab-text">About Me</h1>
                 <img className="pic" src={propic} alt="propic"/>
                 <div className="ab-text2"> 
-                    <p>Hello world, I'm SaaSha👋🏽. 
+                    <p>Hello world, I'm SaaSha. 👋🏽
                         <br/>
                         <br/>
                         I'm a Full-Stack Software Developer based in Miami, FL and Boston, MA. 
-                        Outside of coding, I enjoy hiking high mountains, learning japanese, binge watching anime, practicing yoga, and capoeira flipping.
+                        Outside of coding, I enjoy hiking high mountains, learning japanese, binge watching anime, practicing yoga, and capoeira flipping🤸🏽‍.
                         <br/>
-                        My work is entirely responsive and is inspired by seamless navigation, soft animation, and clean UI.
+                        My work is entirely responsive and is inspired by seamless navigation, cinematography and clean user-friendly UI.
                         {/* I'm truly passionate about making the world a better place with <span className="highlight">Tech</span>, <span className="highlight">Innovation</span>, and <span className="highlight">Open-Source</span>,  
                         by harnessing untapped <span className="highlight">DIVERSITY</span> in the tech field. */}
                         <br/>
