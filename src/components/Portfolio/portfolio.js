11798c7d871716f7186396b926a68f699a3b2384
SaaSha <saasha.pina@gmail.com>
2018-05-15 12:20:43 -0400

    SaaSha Update

--- src/components/Portfolio/portfolio.js
+++ src/components/Portfolio/portfolio.js
@@ -3,8 +3,14 @@ import './portfolio.css';
 import Starwars from './portfolio3.PNG'
 import { Card, Button, CardHeader, CardBody, CardText, Row, Col } from 'reactstrap'
 
+
+const colorblack = {
+  color: 'black'
+}
+
 class Portfolio extends React.Component {
 
+    
     render() {
         return (
         <body className="portfolio-page">
@@ -15,23 +21,25 @@ class Portfolio extends React.Component {
                 <div className="cards">
                   <Row> 
                     <Col sm="4">
-                      <Card className="Card Card1">
-                        <CardHeader tag="h3"> My React Portfolio</CardHeader>
+                      <Card className="Card Card1" >
+                      <a href="https://saashacodes.com" style={{textDecoration:"none"}}> 
+                        <CardHeader tag="h3" style={colorblack}> My React Portfolio</CardHeader>
                           <CardBody>
                             <img className="image" width="100%" src="https://image.ibb.co/dZ6rkd/portfolio.png" alt="Portfolio Card Image"/>
                             <CardText className="text">Created my first React Portfolio, made entirely with ReactJS and reactstrap. Completely Responsive, and easily navigated.</CardText>
                           </CardBody>
-                          <Button className="Button1">Check It Out!</Button>
+                          </a>  
                       </Card>
                     </Col>
                     <Col sm="4">
                       <Card className="Card Card2"> 
-                        <CardHeader tag="h3"> Pure CSS Image </CardHeader>
+                      <a href="https://codepen.io/saashapina/full/wjWVvb/" style={{textDecoration:"none"}}>
+                        <CardHeader tag="h3" style={colorblack}> Pure CSS Image </CardHeader>
                         <CardBody>
                           <img className="image" width="100%" src="https://image.ibb.co/kZT45d/portfolio2.png" alt="Mario Card Image"/>
                           <CardText className="text"> Created a Pure CSS animated Mario on a Nintendo Switch. Almost 700 lines of Pure CSS.</CardText>
                         </CardBody>
-                        <Button className="Button2">Check It Out! </Button>
+                       </a>
                       </Card>
                     </Col>
                     <Col sm="4">
@@ -41,7 +49,7 @@ class Portfolio extends React.Component {
                           <img className="image" width="100%" src={ Starwars } alt="Responsive Cards"/>
                           <CardText className="text"> Created Responsive Reactstrap Cards for Starwars characters taken from SWAPI The Star Wars API</CardText>
                         </CardBody>
-                        <Button className="Button3"> Check It Out!</Button>
+                       
                       </Card>
                     </Col>
                   </Row>
