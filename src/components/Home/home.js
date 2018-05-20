b2955967e04eca745c0e5f5700917572a16e9e73
SaaSha <saasha.pina@gmail.com>
2018-05-20 13:40:21 -0400

    Updating mobile

--- src/components/Home/home.js
+++ src/components/Home/home.js
@@ -14,16 +14,15 @@ class Home extends React.Component {
           <div className="bg"> 
             
               <div className="text-container">
-              <div className="newtext" data-aos="fade-down" data-aos-delay="200">
-                <h1 className="text" style={colorwhite} >SaaSha</h1>
-                <h4 className="text2">Full-Stack Software Developer | Ninja | Life Enthusiast</h4>
+                <div className="newtext" data-aos="fade-down" data-aos-delay="200">
+                  <h1 className="text" style={colorwhite} >SaaSha</h1>
+                  <h4 className="text2">Full-Stack Software Developer | Ninja | Life Enthusiast</h4>
                 </div>
-              </div>
-                <p className="quote" data-aos="fade-down" data-aos-delay="800">i would love to change the world, but they won't give me the source code</p>
-                <div className="wholearrow">
                 <div class="scrolltxt">Scroll Down</div>
                 <div class="arrow bounce"></div>
-                </div>
+
+              </div>
+                <p className="quote" data-aos="fade-down" data-aos-delay="800">i would love to change the world, but they won't give me the source code</p>
           </div>    
         </body>
         )
