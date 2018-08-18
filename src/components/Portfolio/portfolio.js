08ce8227459cdeca79e6c0f29b8682e98ef33113
SaaSha <saasha.pina@gmail.com>
2018-08-17 20:11:21 -0400

    updates

--- src/components/Portfolio/portfolio.js
+++ src/components/Portfolio/portfolio.js
@@ -17,13 +17,13 @@ class Portfolio extends React.Component {
           <div className="p-bg"> 
          
               <div className="p-text-container">
-                <h1 className="p-text size">Portfolio</h1>
+                <h1 className="p-text size">Portfolio<a className=" resumelink" href="https://drive.google.com/open?id=1LNzh8h1SyKKTKrTxxa7JdZZbC735E-43" target="_blank"> (Download Resume)</a></h1>
                 <div className="cards">
 
                   <Row> 
                     <Col sm="4" className="mycard1">
-                      <Card className="Card Card1 fadingin hvr-float-shadow"  /*data-aos="fade-down" data-aos-delay="300"*/>
-                      <a href="https://saashapina.github.io/VinylWaif/" style={{textDecoration:"none"}}> 
+                      <Card className="Card Card1 fadingin hvr-float-shadow"  data-aos="fade-down" data-aos-delay="300">
+                      <a href="https://saashapina.github.io/VinylWaif/" target="_blank" style={{textDecoration:"none"}}> 
                         <CardHeader tag="h3" style={colorblack}>VinylWaif Film Site</CardHeader>
                           <CardBody>
                             <img className="image" width="100%" src="https://preview.ibb.co/fmZBHe/Vinyl_Waif_Pic.png" alt="VinylWaif"/>
@@ -34,8 +34,8 @@ class Portfolio extends React.Component {
                       </Card>
                     </Col>
                     <Col sm="4" className="mycard2">
-                      <Card className="Card Card1 fadingin hvr-float-shadow"  /*data-aos="fade-down" data-aos-delay="300"*/>
-                      <a href="https://www.youtube.com/watch?v=iRNm42evvT0" style={{textDecoration:"none"}}> 
+                      <Card className="Card Card1 fadingin hvr-float-shadow"  data-aos="fade-down" data-aos-delay="600">
+                      <a href="https://www.youtube.com/watch?v=iRNm42evvT0" target="_blank" style={{textDecoration:"none"}}> 
                         <CardHeader tag="h3" style={colorblack}>Lambda Hair School</CardHeader>
                           <CardBody>
                             <img className="image" width="100%" src="https://preview.ibb.co/f6Ycap/Capstone_Slide1.png" alt="Tweet Collage"/>
@@ -46,8 +46,8 @@ class Portfolio extends React.Component {
                       </Card>
                     </Col>
                     <Col sm="4" className="mycard3">
-                      <Card className="Card Card3 fadingin hvr-float-shadow"  /*data-aos="fade-down" data-aos-delay="900" */>
-                      <a href="https://www.youtube.com/watch?v=RlbHlq58GYA" style={{textDecoration:"none"}}>
+                      <Card className="Card Card3 fadingin hvr-float-shadow"  data-aos="fade-down" data-aos-delay="900" >
+                      <a href="https://www.youtube.com/watch?v=RlbHlq58GYA" target="_blank" style={{textDecoration:"none"}}>
                         <CardHeader tag="h3" style={colorblack}> Tweet Collage 🏆 </CardHeader>
                         <CardBody>
                           <img className="image" width="100%" src="https://preview.ibb.co/nfHaT9/slide0.png" alt="Responsive Cards"/>
@@ -61,8 +61,8 @@ class Portfolio extends React.Component {
 
                     <Row className="bottomImageRow">
                     <Col sm="4" className="mycard4">
-                      <Card className="Card Card2 hvr-float-shadow"  /*data-aos="fade-down" data-aos-delay="600" */> 
-                      <a href="https://codepen.io/saashapina/full/wjWVvb/" style={{textDecoration:"none"}}>
+                      <Card className="Card Card2 hvr-float-shadow"  data-aos="fade-down" data-aos-delay="300" > 
+                      <a href="https://codepen.io/saashapina/full/wjWVvb/" target="_blank" style={{textDecoration:"none"}}>
                         <CardHeader tag="h3" style={colorblack}> Animated Pure CSS Image </CardHeader>
                         <CardBody>
                           <img className="image" width="100%" src="https://image.ibb.co/kZT45d/portfolio2.png" alt="Mario Card Image"/>
@@ -72,8 +72,8 @@ class Portfolio extends React.Component {
                       </Card>
                     </Col>
                     <Col sm="4" className="mycard5">
-                      <Card className="Card Card2 hvr-float-shadow" /* data-aos="fade-down" data-aos-delay="600" */> 
-                      <a href="https://peopleofcolorintech.com/interview/saasha-pina-software-engineer/" style={{textDecoration:"none"}}>
+                      <Card className="Card Card2 hvr-float-shadow" data-aos="fade-down" data-aos-delay="600"> 
+                      <a href="https://peopleofcolorintech.com/interview/saasha-pina-software-engineer/" target="_blank" style={{textDecoration:"none"}}>
                         <CardHeader tag="h3" style={colorblack}> Community Contribution: POCIT Article </CardHeader>
                         <CardBody>
                           <img className="image" width="100%" src="https://preview.ibb.co/f1eo39/pocit1.png" alt="POCIT Image"/>
@@ -83,8 +83,8 @@ class Portfolio extends React.Component {
                       </Card>
                     </Col>
                     <Col sm="4" className="mycard6">
-                      <Card className="Card Card2 hvr-float-shadow" /* data-aos="fade-down" data-aos-delay="600" */> 
-                      <a href="https://github.com/saashapina/TweetCollage-TwitterBot" style={{textDecoration:"none"}}>
+                      <Card className="Card Card2 hvr-float-shadow"  data-aos="fade-down" data-aos-delay="900" > 
+                      <a href="https://github.com/saashapina/TweetCollage-TwitterBot" target="_blank" style={{textDecoration:"none"}}>
                         <CardHeader tag="h3" style={colorblack}> Twitter Bot </CardHeader>
                         <CardBody>
                           <img className="image" width="100%" src="https://preview.ibb.co/n4NRiK/twitterbot1.png"  alt="POCIT Image"/>
@@ -95,7 +95,7 @@ class Portfolio extends React.Component {
                       </Card>
                     </Col>
                   </Row>
-
+                  
                 </div>
             </div>
               
